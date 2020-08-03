4107de3a0d0dd6c4293731e3b8cfa4b8521b74fa
PrabhatAgarwal04 <68898829+PrabhatAgarwal04@users.noreply.github.com>
2020-08-03 17:13:09 +0530

    Update vehicleInventory.cpp

--- vehicleInventory.cpp
+++ vehicleInventory.cpp
@@ -1,5 +1,6 @@
 #include "vehicleInventory.h"
    inventory store(telematics s){
-   inventory vehicleStorage = {0,0,0,0}
-   return inventory;
+   inventory vehicleStorage = {0,0,0,0};
+      s={230,motor_temp,30};
+      return inventory;
    }
